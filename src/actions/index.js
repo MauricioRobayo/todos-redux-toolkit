e2cef9dfe68302e0e94e4b50dab869017bad4550
Mauricio Robayo <rfmajo@gmail.com>
2020-07-29 16:39:35 -0500

    Use constants for action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,21 @@
+export const ADD_TODO = 'ADD_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+
 let nextTodoId = 0
 export const addTodo = text => ({
-  type: 'ADD_TODO',
+  type: ADD_TODO,
   id: nextTodoId++,
   text
 })
 
 export const setVisibilityFilter = filter => ({
-  type: 'SET_VISIBILITY_FILTER',
+  type: SET_VISIBILITY_FILTER,
   filter
 })
 
 export const toggleTodo = id => ({
-  type: 'TOGGLE_TODO',
+  type: TOGGLE_TODO,
   id
 })
 
--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,8 +1,8 @@
-import { VisibilityFilters } from '../actions'
+import { VisibilityFilters, SET_VISIBILITY_FILTER } from '../actions'
 
 const visibilityFilter = (state = VisibilityFilters.SHOW_ALL, action) => {
   switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
+    case SET_VISIBILITY_FILTER:
       return action.filter
     default:
       return state
