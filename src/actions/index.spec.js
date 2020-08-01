cd5fdfed08da9c819a47215637a9055b5f910a6d
Mauricio Robayo <rfmajo@gmail.com>
2020-08-01 06:12:57 -0500

    Fix actions tests

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -4,22 +4,24 @@ describe('todo actions', () => {
   it('addTodo should create ADD_TODO action', () => {
     expect(actions.addTodo('Use Redux')).toEqual({
       type: 'ADD_TODO',
-      id: 0,
-      text: 'Use Redux'
+      payload: {
+        id: 0,
+        text: 'Use Redux'
+      }
     })
   })
 
   it('setVisibilityFilter should create SET_VISIBILITY_FILTER action', () => {
     expect(actions.setVisibilityFilter('active')).toEqual({
       type: 'SET_VISIBILITY_FILTER',
-      filter: 'active'
+      payload: 'active'
     })
   })
 
   it('toggleTodo should create TOGGLE_TODO action', () => {
     expect(actions.toggleTodo(1)).toEqual({
       type: 'TOGGLE_TODO',
-      id: 1
+      payload: 1
     })
   })
 })
