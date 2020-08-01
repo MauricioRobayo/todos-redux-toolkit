9f30f76b1f8fc45d85a40af7c1b098f544862d5c
Mauricio Robayo <rfmajo@gmail.com>
2020-08-01 07:43:21 -0500

    Use new todosSlice

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
-import { addTodo } from '../actions'
+import { addTodo } from '../features/todos/todosSlice'
 
-const AddTodo = ({ dispatch }) => {
-  let input
-
+const AddTodo = ({ addTodo }) => {
+  const [todoText, setTodoText] = useState('')
+  const onChange = (e) => setTodoText(e.target.value)
   return (
     <div>
       <form onSubmit={e => {
         e.preventDefault()
-        if (!input.value.trim()) {
+        if (!todoText.trim()) {
           return
         }
-        dispatch(addTodo(input.value))
-        input.value = ''
+        addTodo(todoText)
+        setTodoText('')
       }}>
-        <input ref={node => input = node} />
+        <input type='text' name='todo' value={todoText} onChange={onChange} />
         <button type="submit">
           Add Todo
         </button>
@@ -24,4 +24,8 @@ const AddTodo = ({ dispatch }) => {
   )
 }
 
-export default connect()(AddTodo)
+const mapDispatchToProps = {
+  addTodo
+}
+
+export default connect(null, mapDispatchToProps)(AddTodo)
